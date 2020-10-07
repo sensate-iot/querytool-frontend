749333319c4d84c510f02ca0b73b1f2055252a09
Michel Megens <michel@michelmegens.net>
2020-10-07 16:15:58 +0200

    Remove javascript files.
    Signed-off-by: Michel Megens <michel@michelmegens.net>

--- src/app/dto/orderdirection.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.OrderDirection = void 0;
-var OrderDirection;
-(function (OrderDirection) {
-    OrderDirection["ascending"] = "asc";
-    OrderDirection["descending"] = "desc";
-    OrderDirection["none"] = "";
-})(OrderDirection = exports.OrderDirection || (exports.OrderDirection = {}));
